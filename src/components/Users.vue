c04c73e86073f25f354701d66fb8218b59a557db
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-04 10:41:31 +0300

    Added boostrap.

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,12 +2,24 @@
   <h3>Users:</h3>
 </template>
 <script>
-  export default {
-    name: 'Users',
-  }
+import axios from "axios";
+
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: null,
+    };
+  },
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.users = res.data;
+    });
+  },
+};
 </script>
 <style>
-  h3 {
-    margin-bottom: 5%;
-  }
+h3 {
+  margin-bottom: 5%;
+}
 </style>
